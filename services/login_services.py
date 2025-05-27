2d41745495aeb6ff573d0f32f88ff17b93c21a6a
cales-dev <c.alessio2003@gmail.com>
2025-05-27 21:21:09 +0200

    Added update_user_with_token method

--- services/login_services.py
+++ services/login_services.py
@@ -9,15 +9,31 @@ def get_user_from_db(username, password):
         #seleziono utente
         with dbConnection.cursor() as cursor:
             cursor.execute(
-                "SELECT * FROM tblutenti WHERE username = %s",
+                "SELECT id, username, password FROM tblutenti WHERE username = %s",
                 (username,)
             )
             user = cursor.fetchone()
-            print(user)
             #Controllo password in chiaro corrisponda con hash su db
             if(check_password(password, user['password'])):
-                return user
+                userInfo={"id":user['id'],"username":user['username']} #passo solo info non sensibili
+                return userInfo
             
             return None
+    finally:
+        dbConnection.close()
+
+
+def update_user_with_token(token, refresh, creation_date, id):
+    dbConnection = db_connection.db_connection()
+    try:
+        with dbConnection.cursor() as cursor:
+            cursor.execute(
+                "UPDATE tblutenti SET token=%s, refresh=%s, token_date=%s WHERE id = %s",
+                (token, refresh, creation_date, id)
+            )
+            dbConnection.commit()
+        return True
+    except Exception as ex:
+        return ex 
     finally:
         dbConnection.close()
