cb6209102c74ff212ca8c2839faea6d824f97547
cales-dev <c.alessio2003@gmail.com>
2025-05-27 20:06:32 +0200

    Updated login method

--- routers/login.py
+++ routers/login.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form,HTTPException
+from services import login_services
 
 router = APIRouter()
 
 @router.post("/")
-def login():
-    return "{'message':'ok'}"
+def login(username:str = Form(), password:str = Form()):
+    try:
+        userInfo = login_services.get_user_from_db(username, password)
+
+        if not userInfo:
+            raise HTTPException(status_code=401, detail="Credenziali errate")
+        
+        return {'message':'ok','user':userInfo}
+    except Exception as ex:
+        print(f"errore nel login: {ex}")
+        raise HTTPException(status_code=500, detail="Internal server error")
