d1d8af925413f69f747ba5dd7201f8b72a119350
cales-dev <c.alessio2003@gmail.com>
2025-06-08 17:36:47 +0200

    Added tot_info in ente's query

--- services/report_services.py
+++ services/report_services.py
@@ -16,7 +16,20 @@ def get_summary_data(ente):
                     tblcontratti.fissoresponsabile,
                     tblcontratti.rendicontatoperc,
                     SUM(tblpagamenti.importo_pagato) as totale_importo, 
-                    COUNT(*) as num_verbali
+                    COUNT(*) as num_verbali,
+                            
+                    (SELECT COUNT(*) FROM tblverbali as tblverbali2
+                        JOIN tblpagamenti as tblpagamenti2 ON tblpagamenti2.id = tblverbali2.id 
+                        WHERE tblpagamenti2.Stato LIKE 'Pagato'
+                    ) AS tot_pagati, 
+                    (SELECT COUNT(*) FROM tblverbali as tblverbali2
+                        JOIN tblpagamenti as tblpagamenti2 ON tblpagamenti2.id = tblverbali2.id 
+                        WHERE tblpagamenti2.Stato LIKE 'Da Pagare'
+                    ) AS tot_da_pagare, 
+                    (SELECT COUNT(*) FROM tblverbali as tblverbali2
+                        JOIN tblpagamenti as tblpagamenti2 ON tblpagamenti2.id = tblverbali2.id 
+                        WHERE tblpagamenti2.Stato LIKE 'Rimborso') AS tot_rimborso
+                            
                     FROM tblverbali
                     JOIN tblcontratti ON tblverbali.id = tblcontratti.id
                     JOIN tblpagamenti ON tblpagamenti.id = tblverbali.id
@@ -34,7 +47,7 @@ def get_summary_data(ente):
                 cur.execute("""
                     SELECT 
                     tblverbali.id, 
-                    tblcontratti.rifcomune, 
+                    tblcontratti.rifcomune,
                     SUM(tblpagamenti.importo_pagato) as totale_importo, 
                     COUNT(*) as num_verbali
                     FROM tblverbali
