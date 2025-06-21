5eda6a9f16768d6353628255470852a2a1a955cd
cales-dev <c.alessio2003@gmail.com>
2025-06-21 19:39:58 +0200

    Fixed query in report_services.py

--- services/report_services.py
+++ services/report_services.py
@@ -8,6 +8,7 @@ def get_summary_data(ente):
                 cur.execute("""
                     SELECT 
                     tblverbali.id, 
+                    tblverbali.id_ente as ente_id,  
                     tblcontratti.rifcomune, 
                     tblcontratti.fissoinserito,
                     tblcontratti.fissospedito,
@@ -17,27 +18,32 @@ def get_summary_data(ente):
                     SUM(tblpagamenti.importo_pagato) as totale_importo, 
                     COUNT(*) as num_verbali,
                             
-                    (SELECT COUNT(*) FROM tblverbali as tblverbali2
+                    (SELECT COUNT(*) 
+                        FROM tblverbali as tblverbali2
+                        JOIN tblcontratti as tblcontratti2 ON tblverbali2.id_ente = tblcontratti2.id
                         JOIN tblpagamenti as tblpagamenti2 ON tblpagamenti2.id = tblverbali2.id 
                         WHERE tblpagamenti2.Stato = 'Pagato'
-                        AND tblverbali2.id_ente=tblverbali.id_ente
+                        AND tblcontratti2.rifcomune = tblcontratti.rifcomune
                     ) AS tot_pagati, 
                     (SELECT COUNT(*) FROM tblverbali as tblverbali2
+                        JOIN tblcontratti as tblcontratti2 ON tblverbali2.id_ente = tblcontratti2.id
                         JOIN tblpagamenti as tblpagamenti2 ON tblpagamenti2.id = tblverbali2.id 
                         WHERE tblpagamenti2.Stato = 'Da Pagare'
-                        AND tblverbali2.id_ente=tblverbali.id_ente
+                        AND tblcontratti2.rifcomune = tblcontratti.rifcomune
                     ) AS tot_da_pagare, 
                     (SELECT COUNT(*) FROM tblverbali as tblverbali2
+                        JOIN tblcontratti as tblcontratti2 ON tblverbali2.id_ente = tblcontratti2.id
                         JOIN tblpagamenti as tblpagamenti2 ON tblpagamenti2.id = tblverbali2.id 
                         WHERE tblpagamenti2.Stato = 'Rimborso'
-                        AND tblverbali2.id_ente=tblverbali.id_ente
+                        AND tblcontratti2.rifcomune = tblcontratti.rifcomune
                     )  AS tot_rimborso
                             
                     FROM tblverbali
-                    JOIN tblcontratti ON tblverbali.id = tblcontratti.id
+                    JOIN tblcontratti ON tblverbali.id_ente = tblcontratti.id
                     JOIN tblpagamenti ON tblpagamenti.id = tblverbali.id
                     WHERE tblcontratti.rifcomune = %s
-                    GROUP BY tblverbali.id, 
+                    GROUP BY tblverbali.id_ente, 
+                    tblverbali.id, 
                     tblcontratti.rifcomune, 
                     tblcontratti.fissoinserito,
                     tblcontratti.fissospedito,
@@ -49,15 +55,16 @@ def get_summary_data(ente):
             else:
                 cur.execute("""
                     SELECT 
-                    tblverbali.id, 
-                    tblcontratti.rifcomune,
-                    SUM(tblpagamenti.importo_pagato) as totale_importo, 
-                    COUNT(*) as num_verbali
+                        tblverbali.id_ente as ente_id, 
+                        tblcontratti.rifcomune,
+                        SUM(tblpagamenti.importo_pagato) AS totale_importo, 
+                        COUNT(tblverbali.id) AS num_verbali
                     FROM tblverbali
-                    JOIN tblcontratti ON tblverbali.id = tblcontratti.id
+                    JOIN tblcontratti ON tblverbali.id_ente = tblcontratti.id  
                     JOIN tblpagamenti ON tblpagamenti.id = tblverbali.id
-                    GROUP BY tblverbali.id, 
-                    tblcontratti.rifcomune
+                    GROUP BY 
+                        tblverbali.id_ente,
+                        tblcontratti.rifcomune
                 """)
 
             results = cur.fetchall()
@@ -67,16 +74,16 @@ def get_summary_data(ente):
 
 def get_details_data(ente):
     dbConnection = db_connection.db_connection()
-    print(ente)
     try:
         with dbConnection.cursor() as cur:
             cur.execute("""
                 SELECT 
                 *
-                FROM tblverbali
-                JOIN tblpagamenti ON tblpagamenti.id = tblverbali.id
+                FROM tblpagamenti
+                JOIN tblverbali ON tblpagamenti.id_verbale = tblverbali.id
                 JOIN tblspese ON tblspese.id_verbale = tblverbali.id
                 WHERE tblverbali.id_ente = %s
+                ORDER BY tblverbali.id ASC
             """, 
             (ente,))
 
